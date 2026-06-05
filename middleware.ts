3f93523982a5299c370c39f6e8a434ce1a686d7c
ethanfrost <ethaanfroost@gmail.com>
2026-06-05 10:25:47 -0400

    'latest commit'

--- middleware.ts
+++ middleware.ts
@@ -23,41 +23,41 @@
 //   ],
 // };
 
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-// import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-// const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about(.*)"]);
 
-// const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
-// export default clerkMiddleware(async (auth, req) => {
-//   const { userId } = await auth();
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
 
-//   const isAdminUser = userId === process.env.ADMIN_USER_ID;
+  const isAdminUser = userId === process.env.ADMIN_USER_ID;
 
-//   if (isAdminRoute(req) && !isAdminUser) {
-//     return NextResponse.redirect(new URL("/", req.url));
-//   }
+  if (isAdminRoute(req) && !isAdminUser) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 
-//   if (!isPublicRoute(req)) {
-//     await auth.protect();
-//   }
-// });
-
-// export const config = {
-//   matcher: [
-//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-//     "/(api|trpc)(.*)",
-//   ],
-// };
-
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico)).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
   ],
 };
+
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: [
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico)).*)",
+//     "/(api|trpc)(.*)",
+//   ],
+// };
