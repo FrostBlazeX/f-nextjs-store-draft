bd9087f8d417017b54b6ea29b379210bcb3bcfac
ethanfrost <ethaanfroost@gmail.com>
2026-06-11 12:34:08 -0400

    cart page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,20 +1,3 @@
-// import ProductsContainer from "@/components/products/ProductsContainer";
-
-// async function ProductsPage({
-//   searchParams,
-// }: {
-//   searchParams: {
-//     layout?: string;
-//     search?: string;
-//   };
-// }) {
-//   const layout = searchParams.layout || "grid";
-//   const search = searchParams.search || "";
-
-//   return <ProductsContainer layout={layout} search={search} />;
-// }
-
-// export default ProductsPage;
 import ProductsContainer from "@/components/products/ProductsContainer";
 
 async function ProductsPage({
