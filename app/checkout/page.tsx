653771c3935f91ac5df819ee43fffbdd7d01fc83
ethanfrost <ethaanfroost@gmail.com>
2026-06-15 10:13:38 -0400

    'checkout changes'

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,48 @@
+// "use client";
+// import axios from "axios";
+// import { useSearchParams } from "next/navigation";
+// import React, { useCallback } from "react";
+// import { loadStripe } from "@stripe/stripe-js";
+// import {
+//   EmbeddedCheckoutProvider,
+//   EmbeddedCheckout,
+// } from "@stripe/react-stripe-js";
+
+// const stripePromise = loadStripe(
+//   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string,
+// );
+
+// function CheckoutPage() {
+//   const searchParams = useSearchParams();
+
+//   const orderId = searchParams.get("orderId");
+//   const cartId = searchParams.get("cartId");
+
+//   const fetchClientSecret = useCallback(async () => {
+//     const response = await axios.post("/api/payment", {
+//       orderId,
+//       cartId,
+//     });
+//     return response.data.clientSecret;
+//   }, []);
+
+//   const options = { fetchClientSecret };
+
+//   return (
+//     <div id="checkout">
+//       <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+//         <EmbeddedCheckout />
+//       </EmbeddedCheckoutProvider>
+//     </div>
+//   );
+// }
+// export default CheckoutPage;
+
 "use client";
+
 import axios from "axios";
+import { Suspense, useCallback } from "react";
 import { useSearchParams } from "next/navigation";
-import React, { useCallback } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import {
   EmbeddedCheckoutProvider,
@@ -12,7 +53,7 @@ const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string,
 );
 
-function CheckoutPage() {
+function CheckoutContent() {
   const searchParams = useSearchParams();
 
   const orderId = searchParams.get("orderId");
@@ -23,17 +64,26 @@ function CheckoutPage() {
       orderId,
       cartId,
     });
-    return response.data.clientSecret;
-  }, []);
 
-  const options = { fetchClientSecret };
+    return response.data.clientSecret;
+  }, [orderId, cartId]);
 
   return (
     <div id="checkout">
-      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+      <EmbeddedCheckoutProvider
+        stripe={stripePromise}
+        options={{ fetchClientSecret }}
+      >
         <EmbeddedCheckout />
       </EmbeddedCheckoutProvider>
     </div>
   );
 }
-export default CheckoutPage;
+
+export default function CheckoutPage() {
+  return (
+    <Suspense fallback={<div>Loading checkout...</div>}>
+      <CheckoutContent />
+    </Suspense>
+  );
+}
